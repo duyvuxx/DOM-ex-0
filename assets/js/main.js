45560ee4764d10884c7101ef84a3b7792e7c710c
duyvuxx <duy.vux88@gmail.com>
2023-06-20 13:03:56 +0700

    fix and optimize

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,27 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-$$(".tab-items")[0].style.opacity = 1;        //make first item opacity
+const tabs = $$(".tab-items");
+const panes = $$(".infor-items");
 
-function changeContent(type, element) {
-  var tabs = $$(".tab-items");
-  console.log(`🚀 | changeContent | tabs[0]:`, tabs[0]);
+const tabActive = $(".tab-items.active");
+const line = $(".bar");
 
-  for (var i = 0; i < tabs.length; i++) {
-    tabs[i].style.opacity = 0.6;              // opacity for tab items
-  }
-  
-  element.style.opacity = 1;             
+line.style.left = tabActive.offsetLeft + "px";
+line.style.width = tabActive.offsetWidth + "px";
 
-  var bar = $(".bar");
+tabs.forEach((val, idx) => {
+  val.onclick = function () {
+    $(".tab-items.active").classList.remove("active"); //xóa class active 
+    $(".infor-items.active").classList.remove("active");
 
-  $("#" + type).style.display = "block";      // display content
+    const pane = panes[idx]; //lấy ra nội dung pane ứng với mỗi tab
 
-  switch (type) {
-    case "react":
-      $("#angular").style.display = "none";
-      $("#ember").style.display = "none";
-      $("#vue").style.display = "none";
-      bar.style.width = tabs[0].offsetWidth + "px";
-      bar.style.left = tabs[0].offsetLeft;
-      break;
-    case "angular":
-      $("#react").style.display = "none";
-      $("#ember").style.display = "none";
-      $("#vue").style.display = "none";
-      bar.style.width = tabs[1].offsetWidth + "px";
-      bar.style.left = tabs[1].offsetLeft + "px";
-      break;
-    case "ember":
-      $("#angular").style.display = "none";
-      $("#react").style.display = "none";
-      $("#vue").style.display = "none";
-      bar.style.width = tabs[2].offsetWidth + "px";
-      bar.style.left = tabs[2].offsetLeft + "px";
-      break;
-    default:
-      $("#angular").style.display = "none";
-      $("#react").style.display = "none";
-      $("#ember").style.display = "none";
-      bar.style.width = tabs[3].offsetWidth + "px";
-      bar.style.left = tabs[3].offsetLeft + "px";
-  }
-}
+    line.style.left = this.offsetLeft + "px";
+    line.style.width = this.offsetWidth + "px";
+
+    this.classList.add("active"); //add class active vào el được click
+    pane.classList.add("active"); //add class active vào el pane tương tứng với tab khi click
+  };
+});
+
